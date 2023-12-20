78eee51e0eae86949523cea8eb394c89855ef882
maher2603 <maher2603@hotmail.co.uk>
2023-12-20 17:44:03 +0000

    map and game selection
    changes to the menu and script that allow for different game modes on different maps

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,21 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+public class GameModeManager : MonoBehaviour
+{
+    public static bool Practice { get; private set; }
+    public static bool Race { get; private set; }
+    public static bool Story { get; private set; }
+
+    public static void SetGameModes(bool isPractice, bool isRace, bool isStory)
+    {
+        Practice = isPractice;
+        Race = isRace;
+        Story = isStory;
+    }
+}
+
 public class MainMenu : MonoBehaviour
 {
-    public void ExitButton() {
+    public void ExitButton()
+    {
         Application.Quit();
         Debug.Log("Game Closed");
     }
+
     public void StartGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("GameMenu");
     }
 
     public void ReturnMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Practice()
+    {
+        SceneManager.LoadScene("MapMenu");
+        GameModeManager.SetGameModes(true, false, false);
+    }
+
+    public void Race()
+    {
+        SceneManager.LoadScene("MapMenu");
+        GameModeManager.SetGameModes(false, true, false);
+    }
+
+    public void Story()
+    {
+        SceneManager.LoadScene("StoryGame");
+        GameModeManager.SetGameModes(false, false, true);
+    }
+
+    public void OvalMap()
+    {
+        if (GameModeManager.Practice)
+        {
+            SceneManager.LoadScene("OvalPracticeGame");
+        }
+        else if (GameModeManager.Race)
+        {
+            SceneManager.LoadScene("OvalRaceGame");
+        }
+        else
+        {
+            StartGame();
+        }
     }
 }
